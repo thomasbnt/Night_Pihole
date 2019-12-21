17af0eab36a370925378aba6aac5c9bdac9f721b
arandomtim <59022579+arandomtim@users.noreply.github.com>
2019-12-21 13:36:32 +0000

    Update install.sh
    Try another approach to deleting and copying files - the old one didn't work

--- install.sh
+++ install.sh
@@ -19,36 +19,34 @@ echo -n "# Do you want to install this theme? Y/N or CTRL+C: "
 read answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y"  ] || [ "$answer" = "Yes"  ] || [ "$answer" = "yes"  ] || [ "$answer" = "YES" ]; then
 cd /var/www/html/admin/style/vendor
-rm -f ../skin-blue.min.css
-cp skin-blue.min.css ../
-rm -f ../AdminLTE.min.css
-cp AdminLTE.min.css ../
-cp custom.css ../
-cd /var/www/html/admin/scripts/pi-hole/js
-rm -f ../settings.js
-cp settings.js ../
-rm -f ../network.js
-cp network.js ../
-cd /var/www/html/admin
-rm -f ../settings.php
-cp settings.php ../
-rm -f ../queries.php
-cp queries.php ../
-rm -f ../db_queries.php
-cp db_queries.php ../
-rm -f ../network.php
-cp network.php ../
+rm -f /var/www/html/admin/style/vendor/skin-blue.min.css
+cp skin-blue.min.css /var/www/html/admin/style/vendor/
+rm -f /var/www/html/admin/style/vendor/AdminLTE.min.css
+cp AdminLTE.min.css /var/www/html/admin/style/vendor/
+cp custom.css /var/www/html/admin/style/vendor/
+rm -f /var/www/html/admin/scripts/pi-hole/js/settings.js
+cp settings.js /var/www/html/admin/scripts/pi-hole/js/
+rm -f /var/www/html/admin/scripts/pi-hole/js/network.js
+cp network.js /var/www/html/admin/scripts/pi-hole/js/
+rm -f /var/www/html/admin/settings.php
+cp settings.php /var/www/html/admin/
+rm -f /var/www/html/admin/queries.php
+cp queries.php /var/www/html/admin/
+rm -f /var/www/html/admin/db_queries.php
+cp db_queries.php /var/www/html/admin/
+rm -f /var/www/html/admin/network.php
+cp network.php /var/www/html/admin/
 
 echo -n "# Do you have arevindh's Pi-hole Speedtest installed? Y/N: "
 read answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y"  ] || [ "$answer" = "Yes"  ] || [ "$answer" = "yes"  ] || [ "$answer" = "YES" ]; then
 #code to copy and prepare speedtest files
 cd /var/www/html/admin
-rm -f ../speedtest.php
-cp speedtest.php ../
+rm -f /var/www/html/admin/speedtest.php
+cp speedtest.php /var/www/html/admin/
 cd /var/www/html/admin/scripts/pi-hole/js
-rm -f ../speedresults.js
-cp speedresults.js ../
+rm -f /var/www/html/admin/scripts/pi-hole/js/speedresults.js
+cp speedresults.js /var/www/html/admin/scripts/pi-hole/js/
 fi
 echo "#Finished, press Ctrl + F5 or Command-Shift-R on your computer to change to the new theme ! "
 
